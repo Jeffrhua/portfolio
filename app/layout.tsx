8eee31cb34df2cac0958e88fdeba613e12157cf2
Jeffrhua <Jeffrhua@gmail.com>
2026-02-17 15:14:27 -0800

    Fixing media routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeInit } from "../.flowbite-react/init";
 import "./globals.css";
-import "flowbite/dist/flowbite.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
